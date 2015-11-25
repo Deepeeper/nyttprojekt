e28a06414035fe4bd1a7b6f23126893d05e2672b
Deepeeper <albin.pettersson@hotmail.com>
2015-11-25 14:47:45 +0100

    Ett nytt försök

---
+++ NyApp/NyApp/www/scripts/Controller/coworkerController.js
@@ -0,0 +1,23 @@
+﻿app.controller('coworkerController', ['$scope', function ($scope) {
+    $scope.cardsDone = 2;
+
+    var dateFromFile = new Date("12/31/2015");
+    var currentDate = new Date("1/1/2016");
+
+    $scope.checkDate = function(){
+        console.log(dateFromFile);
+        console.log(currentDate);
+        var timeDiff = Math.abs(currentDate.getTime() - dateFromFile.getTime());
+        var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        return diffDays;
+    };
+
+    $scope.readFiles = function(){
+        readFileDate2();
+    }
+
+    $scope.makeFile = function () {
+        makeFile();
+     
+    }
+}]);
---
+++ NyApp/NyApp/www/scripts/Controller/routes.js
@@ -0,0 +1,43 @@
+﻿
+app.config(function ($routeProvider) {
+    $routeProvider
+
+      .when('/', {
+          templateUrl: 'PartialViews/main.html',
+          controller: 'mainController'
+
+      })
+
+     .when('/leader', {
+          templateUrl: 'PartialViews/leader.html',
+          controller: 'leaderController'
+
+     })
+
+    .when('/coworker', {
+        templateUrl: 'PartialViews/coworker.html',
+        controller: 'coworkerController'
+    })
+
+        .when('/trainingCard1', {
+        templateUrl: 'PartialViews/trainingCard1.html',
+        controller: 'trainingCardController'
+        })
+
+      .when('/trainingCard2', {
+          templateUrl: 'PartialViews/trainingCard2.html',
+          controller: 'trainingCardController'
+      })
+
+      .when('/trainingCard3', {
+          templateUrl: 'PartialViews/trainingCard3.html',
+          controller: 'trainingCardController'
+      })
+
+      .when('/trainingCard4', {
+          templateUrl: 'PartialViews/trainingCard4.html',
+          controller: 'trainingCardController'
+      })
+
+});
+
