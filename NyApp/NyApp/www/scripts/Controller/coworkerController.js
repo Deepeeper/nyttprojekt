fa6b697e8dfbe12547566376e3da2932723b37f2
killinger <hnrk.prsn@gmail.com>
2015-11-27 13:24:17 +0100

    dsfcgscf

--- NyApp/NyApp/www/scripts/Controller/coworkerController.js
+++ NyApp/NyApp/www/scripts/Controller/coworkerController.js
@@ -1,44 +1,35 @@
 ﻿app.controller('coworkerController', function ($scope, $cordovaGlobalization, $cordovaFile, $q) {
 
     var DEBUG = 1;
-    var USEDEBUGDATES = false;
+    var USEDEBUGDATEFROMFILE = false;
+    var USEDEBUGDATECURRENT = false;
     var DEBUGDATEFROMFILE = new Date("1/1/2015");
     var DEBUGCURRENTDATE = new Date("1/1/2015");
 
     document.addEventListener("deviceready", onDeviceReady, false);
     var currentDate;
     var dateFromFile;
+    var dayDifferential = 0;
+
     function onDeviceReady() {
 
-        alert("coworker Controller");
-            var test = $cordovaFile.readAsText(cordova.file.dataDirectory, "date.txt").then(
-                readAsTextSuccess, failCallback);
+        var readAsTextPromise = $cordovaFile.readAsText(cordova.file.dataDirectory, "date.txt").then(
+            readAsTextSuccess, failCallback);
+        var dateToStringPromise = $cordovaGlobalization.dateToString(new Date(), { formatLength: 'short', selector: 'date' }).then(
+            dateToStringSuccess, failCallback);
 
-            var testt = $cordovaGlobalization.dateToString(new Date(), { formatLength: 'short', selector: 'date' }).then(
-                dateToStringSuccess, failCallback);
-
-        $q.all([test, testt]).then(function () {
+        $q.all([readAsTextPromise, dateToStringPromise]).then(function () {
             var timeDiff = Math.abs(currentDate.getTime() - dateFromFile.getTime());
-            var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-            alert(diffDays);
+            dayDifferential = Math.ceil(timeDiff / (1000 * 3600 * 24));
         })
 
-        // Promise test
-        //if(!USEDEBUGDATES) {
-        //$cordovaFile.readAsText(cordova.file.dataDirectory, "date.txt")
-        //    .then(readAsTextSuccess, failCallback).then(
-        //    $cordovaGlobalization.dateToString(new Date(), { formatLength: 'short', selector: 'date' }, failCallback)
-        //    .then(dateToStringSuccess, failCallback));
-        //} else { dateFromFile = DEBUGDATEFROMFILE; currentDate = DEBUGCURRENTDATE; }
-
-        // ngCordova callback functions
         function readAsTextSuccess(result) {
             dateFromFile = new Date(JSON.stringify(result));
-            if (DEBUG == 1) { console.log("1");alert("Success(rAT)!\ndateFromFile = " + dateFromFile + "\nresult = " + JSON.stringify(result)); }
+            if (DEBUG == 1) { console.log("1"); alert("Success(rAT)!\ndateFromFile = " + dateFromFile + "\nresult = " + JSON.stringify(result)); }
         }
         function dateToStringSuccess(result) {
             currentDate = new Date(result.value);
-            if (DEBUG == 1) { console.log("2");alert("Success(dTS)!\ncurrentDate = " + currentDate + "\nresult.value = " + result.value); }
+            if (DEBUG == 1) { console.log("2"); alert("Success(dTS)!\ncurrentDate = " + currentDate + "\nresult.value = " + result.value); }
         }
         function failCallback(error) {
             alert(JSON.stringify(error, null, 4));
@@ -54,11 +45,8 @@
 
 
         $scope.checkDate = function () {
-            console.log(dateFromFile);
-            console.log(currentDate);
-            var timeDiff = Math.abs(currentDate.getTime() - dateFromFile.getTime());
-            var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-            return diffDays;
+            alert("dayDifferntial: " + dayDifferential);
+            return dayDifferential;
         };
 
         $scope.checkCard = function () {
