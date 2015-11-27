a1e1eb4ed37dbb8e2beec1aebd376d5fdfe70b2e
killinger <hnrk.prsn@gmail.com>
2015-11-27 14:39:57 +0100

    Snygg

--- NyApp/NyApp/www/scripts/Controller/coworkerController.js
+++ NyApp/NyApp/www/scripts/Controller/coworkerController.js
@@ -10,28 +10,29 @@
     // asdf
     var currentDate;
     var dateFromFile;
-    var dayDelta = 0;
+    $scope.dayDelta = 0;
+    $scope.cardsDone = 0;
 
     document.addEventListener("deviceready", onDeviceReady, false);
     function onDeviceReady() {
-        // Wrap ngCordova services in promises
+        // Wrap the ngCordova services for file access and date fetching in promises
         var fetchDateFromFilePromise = $cordovaFile.readAsText(cordova.file.dataDirectory, "date.txt").then(
             fetchDateFromFileSuccess, failCallback);
         var fetchDateFromAPIPromise = $cordovaGlobalization.dateToString(new Date(), { formatLength: 'short', selector: 'date' }).then(
             fetchDateFromAPISuccess, failCallback);
 
+        // Fetch number of completed cards from file
+        $cordovaFile.readAsText(cordova.file.dataDirectory, "jort.txt").then(
+            fetchCompletedCardsSuccess, failCallback);
+
         // Only attempt operations on Date objects after they've been succesfully fetched
-        console.log("daydelta 1:" + dayDelta);
         $q.all([fetchDateFromFilePromise, fetchDateFromAPIPromise]).then(function () {
-            console.log("2:");
-            if (USEDEBUGDATEFROMFILE) { dateFromFile = DEBUGDATEFROMFILE; console.log("datefomrfile: " + dateFromFile); }
-            if (USEDEBUGDATECURRENT) { currentDate = DEBUGCURRENTDATE; console.log("currentDate: " + currentDate); }
+            if (USEDEBUGDATEFROMFILE) { dateFromFile = DEBUGDATEFROMFILE; }
+            if (USEDEBUGDATECURRENT) { currentDate = DEBUGCURRENTDATE; }
             var timeDiff = Math.abs(currentDate.getTime() - dateFromFile.getTime());
-            console.log("daydelta 2:" + dayDelta);
-            dayDelta = Math.ceil(timeDiff / (1000 * 3600 * 24));
-            console.log("daydelta 3:" + dayDelta);
+            $scope.dayDelta = Math.ceil(timeDiff / (1000 * 3600 * 24));
         })
-        console.log("daydelta 4:" + dayDelta);
+
         // Callback functions
         function fetchDateFromFileSuccess(result) {
             dateFromFile = new Date(JSON.stringify(result));
@@ -41,26 +42,12 @@
             currentDate = new Date(result.value);
             if (DEBUG == 1) { console.log("1"); }
         }
-        function failCallback(error) {
-            alert(JSON.stringify(error, null, 4));
-        }
-
-        // Fetch number of completed cards from file
-        $cordovaFile.readAsText(cordova.file.dataDirectory, "jort.txt").then(function (result) {
+        function fetchCompletedCardsSuccess(result) {
             $scope.cardsDone = result;
             if (DEBUG == 1) { alert(JSON.stringify(result)); }
-        }, function (error) {
-            alert("läsfel");
-            $scope.cardsDone = 0;
-        });
-
-        $scope.checkDate = function () {
-            console.log("daydelta 5:" + dayDelta);
-            return dayDelta;
-        };
-
-        $scope.checkCard = function () {
-            return getCardValue();
+        }
+        function failCallback(error) {
+            alert(JSON.stringify(error, null, 4));
         }
 
         $scope.previous = function () {
