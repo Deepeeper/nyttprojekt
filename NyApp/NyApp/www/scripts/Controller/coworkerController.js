21eec7cdc0c1662bb4964f07be63ca960e8ddb11
Deepeeper <albin.pettersson@hotmail.com>
2015-11-27 10:10:58 +0100

    till iphone

--- NyApp/NyApp/www/scripts/Controller/coworkerController.js
+++ NyApp/NyApp/www/scripts/Controller/coworkerController.js
@@ -1,19 +1,24 @@
 ﻿app.controller('coworkerController', function ($scope, $cordovaGlobalization, $cordovaFile) {
 
     var DEBUG = 1;
+    var USEDEBUGDATEFROMFILE = false;
+    var USEDEBUGCURRENTDATE = false;
+    var DEBUGDATEFROMFILE = "";
+    var DEBUGCURRENTDATE = false;
 
     document.addEventListener("deviceready", onDeviceReady, false);
     var currentDate;
     var dateFromFile;
     function onDeviceReady() {
         alert("coworker Controller");
-        $cordovaFile.readAsText(cordova.file.dataDirectory, "date.txt").then(function (result) {
-            dateFromFile = new Date(JSON.stringify(result));
-            if(DEBUG == 1) { alert("Success(rAT)!\ndateFromFile = " + dateFromFile + "\nresult = " + JSON.stringify(result)); }
-        }, function (error) {
-            alert("läsfel: " + error);
-        });
-
+        if (!USEDEBUGDATEFROMFILE) {
+            $cordovaFile.readAsText(cordova.file.dataDirectory, "date.txt").then(function (result) {
+                dateFromFile = new Date(JSON.stringify(result));
+                if (DEBUG == 1) { alert("Success(rAT)!\ndateFromFile = " + dateFromFile + "\nresult = " + JSON.stringify(result)); }
+            }, function (error) {
+                alert("läsfel: " + error);
+            });
+        }
         $cordovaGlobalization.dateToString(new Date(), { formatLength: 'short', selector: 'date' }).then(
             function (result) {
                 currentDate = new Date(result.value);
@@ -24,36 +29,33 @@
             });
         document.getElementById('lastest').addEventListener('click', test, false);
 
-        function test() {
+        //Reading the file at coworker startuo
             $cordovaFile.readAsText(cordova.file.dataDirectory, "jort.txt").then(function (result) {
-                if (DEBUG == 1) { alert(JSON.stringify(result)); }
+                if (DEBUG == 1) { alert(JSON.stringify(result)); $scope.cardsDone = result; }
             }, function (error) {
                 alert("läsfel");
+                $scope.cardsDone = 0;
             });
+
+        //if (DEBUG == 1) {
+        //    alert("checkDate = " + checkDate);
+        //}
+
+        $scope.checkDate = function () {
+            console.log(dateFromFile);
+            console.log(currentDate);
+            var timeDiff = Math.abs(currentDate.getTime() - dateFromFile.getTime());
+            var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            return diffDays;
+        };
+
+        $scope.checkCard = function () {
+            return getCardValue();
         }
-        if(DEBUG == 1) { alert("checkDate = " + checkDate());
-    }
 
-    $scope.initCardValue = function () {
-        $scope.cardsDone = getCardValue();
-    }
-
-    
-    
-
-    $scope.checkDate = function(){
-        console.log(dateFromFile);
-        console.log(currentDate);
-        var timeDiff = Math.abs(currentDate.getTime() - dateFromFile.getTime());
-        var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-        return diffDays;
-    };
-
-    $scope.checkCard = function () {
-        return getCardValue();
-    }
-
-    $scope.previous = function () {
-        goBack();
+        $scope.previous = function () {
+            goBack();
+        }
     }
 });
+
