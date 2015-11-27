9834a76c37debf1cddb020eb9e919ab2fb95f70c
Deepeeper <albin.pettersson@hotmail.com>
2015-11-27 14:26:00 +0100

    Datum + trainingcards

--- NyApp/NyApp/www/scripts/Controller/coworkerController.js
+++ NyApp/NyApp/www/scripts/Controller/coworkerController.js
@@ -1,7 +1,7 @@
 ﻿app.controller('coworkerController', function ($scope, $cordovaGlobalization, $cordovaFile, $q) {
 
     // Debug toggles
-    var DEBUG = 0;
+    var DEBUG = 1;
     var USEDEBUGDATEFROMFILE = true;
     var USEDEBUGDATECURRENT = true;
     var DEBUGDATEFROMFILE = new Date("1/1/2015");
@@ -21,21 +21,25 @@
             fetchDateFromAPISuccess, failCallback);
 
         // Only attempt operations on Date objects after they've been succesfully fetched
+        console.log("daydelta 1:" + dayDelta);
         $q.all([fetchDateFromFilePromise, fetchDateFromAPIPromise]).then(function () {
-            if(!USEDEBUGDATEFROMFILE) { dateFromFile = DEBUGDATEFROMFILE; }
-            if(!USEDEBUGDATECURRENT) { currentDate = DEBUGCURRENTDATE; }
+            console.log("2:");
+            if (USEDEBUGDATEFROMFILE) { dateFromFile = DEBUGDATEFROMFILE; console.log("datefomrfile: " + dateFromFile); }
+            if (USEDEBUGDATECURRENT) { currentDate = DEBUGCURRENTDATE; console.log("currentDate: " + currentDate); }
             var timeDiff = Math.abs(currentDate.getTime() - dateFromFile.getTime());
+            console.log("daydelta 2:" + dayDelta);
             dayDelta = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            console.log("daydelta 3:" + dayDelta);
         })
-
+        console.log("daydelta 4:" + dayDelta);
         // Callback functions
         function fetchDateFromFileSuccess(result) {
             dateFromFile = new Date(JSON.stringify(result));
-            if (DEBUG == 1) { console.log("1"); alert("Success(rAT)!\ndateFromFile = " + dateFromFile + "\nresult = " + JSON.stringify(result)); }
+            if (DEBUG == 1) { console.log("1"); }
         }
         function fetchDateFromAPISuccess(result) {
             currentDate = new Date(result.value);
-            if (DEBUG == 1) { console.log("2"); alert("Success(dTS)!\ncurrentDate = " + currentDate + "\nresult.value = " + result.value); }
+            if (DEBUG == 1) { console.log("1"); }
         }
         function failCallback(error) {
             alert(JSON.stringify(error, null, 4));
@@ -43,13 +47,15 @@
 
         // Fetch number of completed cards from file
         $cordovaFile.readAsText(cordova.file.dataDirectory, "jort.txt").then(function (result) {
-            if (DEBUG == 1) { alert(JSON.stringify(result)); $scope.cardsDone = result; }
+            $scope.cardsDone = result;
+            if (DEBUG == 1) { alert(JSON.stringify(result)); }
         }, function (error) {
             alert("läsfel");
             $scope.cardsDone = 0;
         });
 
         $scope.checkDate = function () {
+            console.log("daydelta 5:" + dayDelta);
             return dayDelta;
         };
 
