7c4fa62cf2985c82e64ded9cf2e18f5369fecafb
killinger <hnrk.prsn@gmail.com>
2015-12-03 11:42:59 +0100

    testste

--- NyApp/NyApp/www/scripts/Controller/mainController.js
+++ NyApp/NyApp/www/scripts/Controller/mainController.js
@@ -5,8 +5,22 @@
         $cordovaFile.checkFile(cordova.file.dataDirectory, "date.txt").then(
             resultCallback,
             function (error) {
-                $cordovaGlobalization.dateToString(new Date(), { formatLength: 'short', selector: 'date' }).then(function (result) {
-                    $cordovaFile.writeFile(cordova.file.dataDirectory, "date.txt", result.value, true).then(resultCallback);
+                $cordovaGlobalization.dateToString(new Date(), { formatLength: 'short', selector: 'date' }).then(function (date) {
+                    $cordovaFile.writeFile(cordova.file.dataDirectory, "date.txt", date.value, true).then(resultCallback);
+
+                    var currentTime = new Date(date.value).getTime();
+                    var scheduleDate = new Date(currentTime + 20 * 1000);
+                    $cordovaLocalNotification.schedule({
+                        id: 1,
+                        title: 'Kort 1 schemalagt',
+                        text: 'pigeon cum does not taste bitter',
+                        at: scheduleDate,
+                        data: {
+                            // customProperty: 'custom value'
+                        }
+                    }).then(function (result) {
+                        alert(JSON.stringify(result, null, 4));
+                    });
                 }, resultCallback);
             });
 
