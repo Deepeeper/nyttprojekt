1adac9bee1af1c1f02eee2095260b197f0e28669
killinger <hnrk.prsn@gmail.com>
2015-12-03 12:38:07 +0100

    testnotification

--- NyApp/NyApp/www/scripts/Controller/trainingCardController.js
+++ NyApp/NyApp/www/scripts/Controller/trainingCardController.js
@@ -3,21 +3,47 @@
     function onDeviceReady() {
 
         $cordovaFile.readAsText(cordova.file.dataDirectory, "jort.txt").then(function (result) {
-            alert(JSON.stringify(result)); $scope.cardsDone = result; 
+            alert(JSON.stringify(result)); $scope.cardsDone = result;
         }, function (error) {
             alert("läsfel");
             $scope.cardsDone = 0;
         });
 
-        $scope.saveCardNumber = function(num) {
-                $cordovaFile.writeFile(cordova.file.dataDirectory, "jort.txt", num, true)
-                .then(function (success) {
-                      cardsDone = num;
-                },
-            function (error) {
-                alert(JSON.stringify(error, null, 4));
-            });
+        $scope.saveCardNumber = function (num) {
+            $cordovaFile.writeFile(cordova.file.dataDirectory, "jort.txt", num, true)
+            .then(function (success) {
+                $scope.cardsDone = num;
+            },
+        function (error) {
+            alert(JSON.stringify(error, null, 4));
+        });
         }
+
+        $scope.updateSchedule = function (num) {
+            $cordovaLocalNotification.cancel(num).then(function (result) {
+                alert("Notification " + num + " avbruten");
+            });
+            $cordovaGlobalization.dateToString(new Date(), { formatLength: 'short', selector: 'date' }).
+            then(function (date) {
+                var currentTime = new Date(date.value).getTime();
+                var scheduleDate = new Date(currentTime + 20 * 1000);
+                alert("currentTime: " + currentTime);
+                alert("scheduleDate: " + scheduleDate.getDate());
+                $cordovaLocalNotification.schedule({
+                    id: num+1,
+                    title: 'Kort schemalagt' + (num+1),
+                    text: 'pigeon cum does not taste bitter',
+                    at: scheduleDate,
+                    data: {
+                        // customProperty: 'custom value'
+                    }
+                }).then(function (result) {
+                    alert("kort schemalagt: " + (num+1));
+                });
+            });
+
+        }
+
     }
     $scope.previous = function () {
         window.history.back();
